d380dace410f4686648c7995f3b13a7566e2c4d0
Vyraal1 <kevin.afable@gmail.com>
2020-03-16 21:26:32 -0400

    feat: server prototype

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+server.applyMiddleware({ app });
+
+app.listen({ port }, () => {
+  console.log(`We are live at ${server.graphqlPath}`);
+});
