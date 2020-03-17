98d8dd53d1274941576de0e43561b8dc84a04c1d
Vyraal1 <kevin.afable@gmail.com>
2020-03-16 22:02:50 -0400

    feat: adding id to author

--- schema.js
+++ schema.js
@@ -3,6 +3,7 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type Query {
     hello: String
+    # get all the books in my database and those in Congress
     getBooks: [Book]!
   }
 
@@ -23,6 +24,7 @@ const typeDefs = gql`
   }
 
   type Author {
+    id: ID!
     books: [Book]!
     firstName: String
   }
