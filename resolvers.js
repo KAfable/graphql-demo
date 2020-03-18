c09feb95ea12dd46f529710088788750c151b5cf
Vyraal1 <kevin.afable@gmail.com>
2020-03-17 22:43:21 -0400

    feat: overriding resolver since db field is inaccurate

--- resolvers.js
+++ resolvers.js
@@ -3,13 +3,22 @@ const { books, authors } = require("./models");
 const resolvers = {
   Query: {
     hello: () => true,
-    getBooks: () => []
+    getBooks: () => [],
+    author: (_, args) => authors.find(args.id)
   },
   Mutation: {
-    createAuthor: async name => {
-      console.log(name);
-      const [author] = await authors.add({ first_name: name });
+    createAuthor: async (parent, { firstName, age }, context) => {
+      // console.log(firstName);
+      const [id] = await authors.add({ first_name: firstName, age });
+      const author = await authors.find(id);
       return author;
+    }
+  },
+  Author: {
+    // NORMALLY DONT NEED THIS
+    // but I'm bad when I did my first migrations
+    firstName: parent => {
+      return parent.first_name;
     }
   }
 };
